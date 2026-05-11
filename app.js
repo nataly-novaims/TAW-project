24aaf620ad9527191c3b6fb87b960df05dabb7db
vladi <vladi@NULL.Home>
2026-05-11 11:56:12 +0100

    added event details and updated scripts

--- app.js
+++ app.js
@@ -7,10 +7,12 @@ function renderEvents(containerId, limit = null) {
     const eventsToShow = limit ? myEvents.slice(0,limit) : myEvents;
 
     container.innerHTML = eventsToShow.map(event => `
-        <div class = "event-card">
-            <h4>${event.title}</h4>
-            <p>${event.date}<br>${event.location}</p>
-        </div>
+        <a href="event-details.html? id=${InputEvent.id}" class="event-card-link">
+            <div class = "event-card">
+                <h4>${event.title}</h4>
+                <p>${event.date}<br>${event.location}</p>
+            </div>
+        </a>
     `).join('');
 }
 
