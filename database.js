94c5e6ffe015866dd06ce4b90bd1db3cec4c3634
Uzair0010 <178794534+Uzair0010@users.noreply.github.com>
2026-05-19 17:42:13 +0100

    Add files via upload

---
+++ database.js
@@ -0,0 +1,44 @@
+// simple json file as database (so we don't need to install sqlite/mongo)
+const fs = require('fs');
+const path = require('path');
+
+const FILE = path.join(__dirname, 'users.json');
+
+// create file if it doesn't exist yet
+if (!fs.existsSync(FILE)) {
+    fs.writeFileSync(FILE, JSON.stringify({ users: [], nextId: 1 }));
+}
+
+function read() {
+    return JSON.parse(fs.readFileSync(FILE, 'utf-8'));
+}
+
+function save(data) {
+    fs.writeFileSync(FILE, JSON.stringify(data, null, 2));
+}
+
+function findUser(usernameOrEmail) {
+    const db = read();
+    return db.users.find(u => u.username === usernameOrEmail || u.email === usernameOrEmail);
+}
+
+function findById(id) {
+    const db = read();
+    return db.users.find(u => u.id === id);
+}
+
+function addUser(username, email, hashedPassword) {
+    const db = read();
+    const user = {
+        id: db.nextId,
+        username: username,
+        email: email,
+        password: hashedPassword
+    };
+    db.users.push(user);
+    db.nextId++;
+    save(db);
+    return user;
+}
+
+module.exports = { findUser, findById, addUser };
---
+++ server.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const authRoutes = require('./routes/auth');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api/auth', authRoutes);
+
+app.listen(PORT, () => {
+    console.log('Server running on http://localhost:' + PORT);
+});
